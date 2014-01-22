c00a0b4facc42e0778158d24791c29eab9a4d5cb
Ruben Pla <rubenpla.develop@gmail.com>
2014-01-22 18:10:53 +0100

    -Added button in mainactivity.xml -IMplemented listener for button

--- proyectogit_ejemplo2/src/com/example/proyectogit_ejemplo2/MainActivity.java
+++ proyectogit_ejemplo2/src/com/example/proyectogit_ejemplo2/MainActivity.java
@@ -1,15 +1,33 @@
 package com.example.proyectogit_ejemplo2;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+	Context mContext;
+	Button btn;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		mContext = this;
+
+		btn = (Button) findViewById(R.id.button1);
+		btn.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Toast.makeText(mContext, "Mensaje Toast", Toast.LENGTH_LONG)
+						.show();
+			}
+
+		});
 	}
 
 	@Override
