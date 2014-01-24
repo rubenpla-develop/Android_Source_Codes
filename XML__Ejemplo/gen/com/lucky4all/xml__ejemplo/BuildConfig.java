1bd726f79b0c78bd9f5282fbb67934432a77c237
Ruben Pla Ferrero <rubenpla.develop@gmail.com>
2014-01-24 20:18:58 +0100

    XML_Ejemplo eliminado, Todas las ramas sincronizadas

--- XML__Ejemplo/gen/com/lucky4all/xml__ejemplo/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.lucky4all.xml__ejemplo;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
--- XML__Ejemplo/src/com/lucky4all/xml__ejemplo/HolderLVMain.java
+++
@@ -1,8 +0,0 @@
-package com.lucky4all.xml__ejemplo;
-
-import android.widget.TextView;
-
-public class HolderLVMain {
-	public TextView tvTitulo;
-	// public ImageView tvImagen;
-}
