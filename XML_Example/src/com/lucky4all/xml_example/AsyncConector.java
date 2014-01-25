e7da2abc6e1bb934c62a9947b13097dad39b3f84
Ruben Pla Ferrero <rubenpla.develop@gmail.com>
2014-01-25 12:57:27 +0100

    Creada clase AsyncConector
    Encargada de ejecutar en segundo plano la obtencion de las noticias a
    traves de la clase ConectorHttpXML(no implementada aun)

---
+++ XML_Example/src/com/lucky4all/xml_example/AsyncConector.java
@@ -0,0 +1,42 @@
+package com.lucky4all.xml_example;
+
+import java.util.ArrayList;
+
+import android.os.AsyncTask;
+import android.widget.ArrayAdapter;
+
+public class AsyncConector extends AsyncTask<Void, Void, Void> {
+	
+	private ArrayList<Publicacion> publicaciones;
+	private ArrayAdapter<Publicacion> adapter;
+	private String url;
+	
+	public AsyncConector(ArrayAdapter<Publicacion> adapter, String url) {
+		super();
+		this.adapter = adapter;
+		this.url = url;
+	}
+
+	@Override
+	protected Void doInBackground(Void... params) {
+		// TODO Auto-generated method stub
+		ConectorHttpXML conector = new ConectorHttpXML(url);
+		
+		try{
+				publicaciones = conector.execute();
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	@Override
+	protected void onPostExecute(Void result) {
+		// TODO Auto-generated method stub
+		for (Publicacion tmp : publicaciones)
+			adapter.add(tmp);
+				
+		adapter.notifyDataSetChanged();
+		super.onPostExecute(result);
+	}
+}
